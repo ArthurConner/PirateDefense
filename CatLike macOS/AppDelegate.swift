ff2ae6b3d8c1cac413963ed09a98470cc5dc3664
Arthur <aconner@earthlink.net>
2017-09-21 10:23:40 -0700

    Initial Commit

---
+++ CatLike macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CatLike macOS
+//
+//  Created by Arthur  on 9/21/17.
+//  Copyright © 2017 Arthur . All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
